4a5584042b3517a193bb80f5a919eda78f0b84d0
Om Sakharkar <sakharkarom777@gmail.com>
2024-11-21 18:07:19 +0530

    updated background

--- parrot_interface/tailwind.config.js
+++ parrot_interface/tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        customPurple: '#5F48D9',  
-        customBlue: '#1D0B77',
+        customPurple: '#C5BBFF',  
+        customBlue: '#8F7AFA',
         customDarkBlue: '#180B41',
         customBlack: '#1E1E1E',
         customGray: '#323232'
